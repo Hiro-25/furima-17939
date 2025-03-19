81939b6eff91a9228898c51b1fd04245d614cbb0
Hiro-25 <63336620+Hiro-25@users.noreply.github.com>
2025-03-19 14:43:24 +0900

    submission_commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :nickname, presence: { message: I18n.t('errors.messages.blank') }
   validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: { message: I18n.t('errors.messages.taken') }
+
+  validates :password, presence: true, length: { minimum: 6, message: I18n.t('errors.messages.too_short', count: 6) },
+                       format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: 'is invalid. Include both letters and numbers' }
+
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
+  validates :first_name, presence: true,
+                         format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
+
+  validates :last_name_kana, presence: true,
+                             format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' }
+  validates :first_name_kana, presence: true,
+                              format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' }
+
+  validates :birthday, presence: { message: "can't be blank" }
 end
