b585adb8437b37cb9bdcb02a71c6cc933a779271
Hiro-25 <63336620+Hiro-25@users.noreply.github.com>
2025-03-23 21:19:12 +0900

    submission_commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show]
   # before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
@@ -38,7 +39,6 @@ class ItemsController < ApplicationController
   # end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   private
