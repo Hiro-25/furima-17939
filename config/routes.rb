d3841be7ed7750844288d932ff03e6a10c981fc6
Hiro-25 <63336620+Hiro-25@users.noreply.github.com>
2025-04-01 15:57:39 +0900

    form_commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'orders/index'
+  get 'orders/create'
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :items do
+    resources :orders, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  end,
 
 end
