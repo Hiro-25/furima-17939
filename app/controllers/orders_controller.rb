407761b5a2567a2ad5305ec50f61c01b7694c7e9
Hiro-25 <63336620+Hiro-25@users.noreply.github.com>
2025-04-04 11:39:14 +0900

    step2-2_before_sk_commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,6 +11,12 @@ class OrdersController < ApplicationController
     @order_address = OrderAddress.new(order_address_params)
 
     if @order_address.valid?
+      Payjp.api_key = 'sk_test_' # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: order_params[:price],  # 商品の値段
+        card: order_params[:token],    # カードトークン
+        currency: 'jpy' # 通貨の種類（日本円）
+      )
       @order_address.save
       redirect_to root_path
     else
